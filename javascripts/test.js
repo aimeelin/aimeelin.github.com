4cdbc783dc0c2bfff75e4fbcdd65f7ff5a87bbbd
aimeelin <980860932@qq.com>
2016-01-21 16:41:06 +0800

    Signed-off-by: aimeelin <980860932@qq.com>

---
+++ javascripts/test.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+
+    $(function() {
+		$('.carousel').carousel({
+        interval: 2000
+        });		
+		
+		$('#datetimepicker').datetimepicker({
+	inline:true,
+	lang:'ch',
+	timepicker:false
+});
+		
+	});
+
+	
+	
